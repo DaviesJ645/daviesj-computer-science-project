c37b91cb205a37ca5bcc42246e2e4217dc1b92a3
DaviesJ645 <64731521+DaviesJ645@users.noreply.github.com>
2020-05-17 11:21:54 +0100

    Final project

--- main.ts
+++ main.ts
@@ -5,6 +5,7 @@ namespace SpriteKind {
 }
 sprites.onOverlap(SpriteKind.friendly_projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     info.changeScoreBy(1)
+    Primary.destroy(effects.fire, 100)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     Primary = sprites.createProjectileFromSprite(img`
@@ -24,17 +25,20 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
-`, Player_1, 0, -10)
+`, Player_1, 0, -100)
     Primary.setKind(SpriteKind.friendly_projectile)
 })
 sprites.onOverlap(SpriteKind.enemy_projectile, SpriteKind.Player, function (sprite, otherSprite) {
-    info.changeLifeBy(-1)
+    game.over(false)
+})
+info.onCountdownEnd(function () {
+	
 })
 let enemy_projectile_1: Sprite = null
 let enemy_projectile_2: Sprite = null
 let Primary: Sprite = null
 let Player_1: Sprite = null
-scene.setBackgroundColor(13)
+scene.setBackgroundColor(6)
 Player_1 = sprites.create(img`
 . . . . . . f f f f . . . . . . 
 . . . . f f f 2 2 f f f . . . . 
@@ -54,38 +58,55 @@ Player_1 = sprites.create(img`
 . . . . . f f . . f f . . . . . 
 `, SpriteKind.Player)
 info.setScore(0)
-let input2 = game.askForString("ARE YOU READY ?", 1)
+game.showLongText("Use the d-pad to move and the A button to shoot. One hit and you're done for ! Are you ready ?    Click A to begin ", DialogLayout.Full)
+info.startCountdown(5)
 pause(5000)
 let Boss_level_1 = sprites.create(img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . f f f f f f f f f f f . . 
-. . . f f f f f f f f f f f . . 
-. . . f f 2 1 f f f 1 2 f f . . 
-. . . f f f f f f f f f f f . . 
-. . . 2 2 2 2 2 2 2 2 2 2 2 . . 
-. . . 2 f 1 f 1 f 1 f 1 f 2 . . 
-. . . 2 1 f 1 f 1 f 1 f 1 2 . . 
-. . . 2 2 2 f 1 f 1 f 2 2 2 . . 
-. . . 2 2 2 2 f 1 f 2 2 2 2 . . 
-. . . 2 2 2 2 2 2 2 2 2 2 2 . . 
-. . 2 . 2 . 2 . 2 . 2 . 2 . 2 . 
-. 2 . 2 . 2 . 2 . 2 . 2 . 2 . 2 
-. f . f . f . f . f . f . f . f 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . f f f f . . . . . . . . . . 
+. . . . . . . . f f 1 1 1 1 f f . . . . . . . . 
+. . . . . . . f b 1 1 1 1 1 1 b f . . . . . . . 
+. . . . . . . f 1 1 1 1 1 1 1 1 f . . . . . . . 
+. . . . . . f d 1 1 1 1 1 1 1 1 d f . . . . . . 
+. . . . . . f d 1 1 1 1 1 1 1 1 d f . . . . . . 
+. . . . . . f d d d 1 1 1 1 d d d f . . . . . . 
+. . . . . . f b d b f d d f b d b f . . . . . . 
+. . . . . . f c d c f 1 1 f c d c f . . . . . . 
+. . . . . . . f b 1 1 1 1 1 1 b f . . . . . . . 
+. . . . . . f f f c d b 1 b d f f f f . . . . . 
+. . . . f c 1 1 1 c b f b f c 1 1 1 c f . . . . 
+. . . . f 1 b 1 b 1 f f f f 1 b 1 b 1 f . . . . 
+. . . . f b f b f f f f f f b f b f b f . . . . 
+. . . . . . . . . f f f f f f . . . . . . . . . 
+. . . . . . . . . . . f f f . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
 `, SpriteKind.Enemy)
-Boss_level_1.setPosition(78, 7)
-Player_1.setFlag(SpriteFlag.StayInScreen, false)
+Player_1.setFlag(SpriteFlag.StayInScreen, true)
 controller.moveSprite(Player_1)
 Boss_level_1.setFlag(SpriteFlag.BounceOnWall, true)
 forever(function () {
-    if (Boss_level_1.x == -100 || Boss_level_1.x == 100) {
-        Boss_level_1.x += -5
+    if (info.score() > 50) {
+        for (let index = 0; index < 40; index++) {
+            Boss_level_1.x += 3
+            pause(50)
+        }
+        pause(100)
+        for (let index = 0; index < 40; index++) {
+            Boss_level_1.x += -3
+            pause(50)
+        }
+    } else {
+    	
     }
-    pause(100)
 })
 forever(function () {
-    if (info.score() < 3) {
+    if (info.score() <= 100 && info.score() >= 50) {
         enemy_projectile_2 = sprites.createProjectileFromSprite(img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -105,11 +126,12 @@ forever(function () {
 . . . . . . . . . . . . . . . . 
 `, Boss_level_1, 0, 100)
         enemy_projectile_2.setKind(SpriteKind.enemy_projectile)
-        pause(100)
+        pause(400)
     }
 })
 forever(function () {
-    if (info.score() > 3) {
+    if (info.score() < 50) {
+        Boss_level_1.setPosition(70, 7)
         enemy_projectile_1 = sprites.createProjectileFromSprite(img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -130,5 +152,54 @@ forever(function () {
 `, Boss_level_1, Math.randomRange(-100, 100), 100)
         enemy_projectile_1.setKind(SpriteKind.enemy_projectile)
     }
-    pause(500)
+    pause(600)
+})
+forever(function () {
+    if (info.score() > 100) {
+        enemy_projectile_2 = sprites.createProjectileFromSprite(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . 2 4 2 . . . . . . . 
+. . . . . . 4 f 4 . . . . . . . 
+. . . . . . 2 4 2 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, Boss_level_1, 0, 100)
+        enemy_projectile_2.setKind(SpriteKind.enemy_projectile)
+        pause(100)
+        enemy_projectile_1 = sprites.createProjectileFromSprite(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . e f e f e . . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . . e 4 4 4 e . . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . . e 4 4 4 e . . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . . e f e f e . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, Boss_level_1, Math.randomRange(-100, 100), 100)
+        enemy_projectile_1.setKind(SpriteKind.enemy_projectile)
+        pause(500)
+    }
+})
+forever(function () {
+    if (info.life() <= 0) {
+    	
+    }
 })
